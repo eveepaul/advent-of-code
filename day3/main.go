db4f0b880b79b2b771a9d9de341e6883db871554
Yves <yves@Mays-MacBook-Pro.local>
2023-10-12 19:32:28 +0800

    init solution part 2

--- day3/main.go
+++ day3/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"unicode"
+	// "unicode"
 )
 
 func main() {
@@ -17,56 +17,23 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	var totalRuck int
-
+	elvesGroup := make([]string, 3)
+	var elvesCount int
 	for scanner.Scan() {
 
 		str := scanner.Text()
-		lastHalf := str[len(str)/2:]
-		firstHalf := str[:len(str)/2]
-		fmt.Printf("Line: {%v} {%v} {%v} \n", str, firstHalf, lastHalf)
+		elvesGroup[elvesCount] = str
+		elvesCount++
+		if elvesCount == 3 {
 
-		charCountMap := make(map[string]int)
-		for i := 0; i < len(firstHalf); i++ {
-			_, exists := charCountMap[string(firstHalf[i])]
-			if exists {
-				charCountMap[string(firstHalf[i])] += 1
-			} else {
-				charCountMap[string(firstHalf[i])] = 0
-			}
+			elvesCount = 0
+
+			fmt.Printf("Elves Group: %v \n", elvesGroup)
+
 		}
-
-		for key := range charCountMap {
-			charCountMap[key] = 0
-		}
-
-		for i := 0; i < len(lastHalf); i++ {
-			_, exist := charCountMap[string(lastHalf[i])]
-			if exist {
-				charCountMap[string(lastHalf[i])] += 1
-			}
-		}
-
-    var dup string
-    for key := range charCountMap {
-      if charCountMap[key] > 0 {
-        dup = key
-      }
-    }
-    uppercase := unicode.IsUpper([]rune(dup)[0])
-    var charCode int
-    caseText := "Lowercase"
-    if uppercase {
-      caseText = "Uppercase"
-      charCode = int(dup[0] - 'A' + 27)
-    } else {
-      charCode = int(dup[0] - 'a' + 1)
-    }
-    fmt.Printf("Dup ^ is: %v (%v), code: %v \n", dup, caseText, charCode)
-    totalRuck += charCode
 	}
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal("Error scanning file")
 	}
-  fmt.Printf("Total ruck: %v \n", totalRuck)
+	fmt.Printf("Total ruck: %v \n", totalRuck)
 }
